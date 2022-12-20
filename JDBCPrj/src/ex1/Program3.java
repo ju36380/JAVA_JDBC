9571663af1bf99c4b035e20188d8c26e97d66bfc
Jun_Hyeok <ju36380@naver.com>
2022-12-20 14:15:42 +0900

    [update] 자바 JDBC 프로그래밍 강의 11 = 데이터 수정 구현

---
+++ JDBCPrj/src/ex1/Program3.java
@@ -0,0 +1,47 @@
+package ex1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class Program3 {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		String title = "TEST3";
+		String writerId = "newlec";
+		String content = "hahahaha3";
+		String files = "";
+		int id = 17;
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "UPDATE NOTICE"
+				+ " SET"
+				+ "    TITLE=?,"
+				+ "    CONTENT=?,"
+				+ "    FILES=?"
+				+ "WHERE ID=?";
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "newlec", "1234");
+		//Statement st = con.createStatement();
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, title);
+		st.setString(2, content);
+		st.setString(3, files);
+		st.setInt(4, id);
+		
+		int result = st.executeUpdate();		
+		
+		System.out.println(result);
+		
+		st.close();
+		con.close();
+
+	}
+
+}
